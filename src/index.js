70647b0a4f76eb37014d6adc5a3421c1fafcd39e
Akhila Vijay <48543759+akhilavijay@users.noreply.github.com>
2021-07-29 19:23:35 +0530

    Add files via upload

---
+++ src/index.js
@@ -0,0 +1,12 @@
+import React from "react";
+import ReactDOM from "react-dom";
+// import Heading from "./components/Heading";
+// import List from "./components/List";
+import App from "./components/App";
+
+ReactDOM.render(
+  <div>
+    <App />
+  </div>,
+  document.getElementById("root")
+);
